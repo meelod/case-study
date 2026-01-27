a37d8122539db6ba895006ca8b8885cf2eaeb08a
Meelod Waheed <meelod.waheed360@gmail.com>
2026-01-27 16:06:11 -0500

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import ChatWindow from "./components/ChatWindow";
 
 const App: React.FC = () => {
-  return (
-    <div className="bg-white">
-      <div className="fixed top-0 left-0 w-full bg-white shadow-[0px_2px_4px_rgba(0,0,0,0.1)] flex font-bold text-base justify-center items-center h-[60px]">
-        Instalily Case Study
-      </div>
-      <ChatWindow />
-    </div>
-  );
+    return (
+        <div className="bg-white">
+            <div className="fixed top-0 left-0 w-full bg-white shadow-[0px_2px_4px_rgba(0,0,0,0.1)] flex font-bold text-base justify-center items-center h-[60px]">
+                Instalily Case Study
+            </div>
+            <ChatWindow />
+        </div>
+    );
 };
 
 export default App;
