e48aa751ca0dd4d5f4381756c7c33bcd1726c9f4
Meelod Waheed <meelod.waheed360@gmail.com>
2026-01-28 13:52:02 -0500

    Update ChatWindow.tsx

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -69,7 +69,7 @@ const ChatWindow: React.FC = () => {
                     value={input}
                     onChange={(e) => setInput(e.target.value)}
                     placeholder="Type a message..."
-                    onKeyPress={handleKeyPress}
+                    onKeyDown={handleKeyPress}
                     className="flex-1 px-2.5 py-2.5 mr-2.5 rounded-[5px] border border-[#ccc] text-sm"
                 />
                 <button
